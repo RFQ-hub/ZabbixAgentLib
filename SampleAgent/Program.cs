a2a574d74d0a0e18e2f16ef9bd13a89c9651cb48
Julien Roncaglia <julien@roncaglia.fr>
2013-06-25 14:02:17 +0200

    Add a sample agent project that does nothing

---
+++ SampleAgent/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mono.Options;
+
+namespace SampleAgent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 10051;
+            string activeServer = null;
+            string hostName = "tests";
+            var argumentParser = new OptionSet
+            {
+                {"a=|active-server=", v => activeServer = v},
+                {"p=|port=", (int v) => port = v },
+                {"h=|host=", v => hostName =v }
+            };
+            argumentParser.Parse(args);
+
+
+        }
+    }
+}
