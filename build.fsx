1ece4a794162792ff3ca6170ff7630cc50bc75f2
Julien Roncaglia <julien.roncaglia@itg.com>
2017-10-30 16:24:39 +0100

    Add teamcity build counter in the build number

--- build.fsx
+++ build.fsx
@@ -6,11 +6,28 @@ open Fake
 let configuration = environVarOrDefault "Configuration" (if buildServer = LocalBuild then "Debug" else "Release")
 let solution = "ZabbixAgentLib.sln"
 
+let dotNetAdditionalArgs =
+    match TeamCityHelper.TeamCityBuildParameters.tryGet "build.counter" with
+    | Some buildNumber -> [ sprintf "/p:BuildNumber=%s" buildNumber ]
+    | _ -> []
+
 Target "build" <| fun _ ->
-    DotNetCli.Build (fun p -> { p with WorkingDir = "src"; Configuration = configuration; Project = solution })
+    DotNetCli.Build (fun p ->
+        { p with
+            WorkingDir = "src"
+            Configuration = configuration
+            Project = solution
+            AdditionalArgs = dotNetAdditionalArgs
+        })
 
 Target "pack" <| fun _ ->
-    DotNetCli.Pack (fun p -> { p with WorkingDir = "src"; Configuration = configuration; Project = solution })
+    DotNetCli.Pack (fun p ->
+        { p with
+            WorkingDir = "src"
+            Configuration = configuration
+            Project = solution
+            AdditionalArgs = dotNetAdditionalArgs
+        })
 
     !! ("src\\**\\*.nupkg") |> Seq.iter TeamCityHelper.PublishArtifact
 
