72531194a49f2d252ba317a719e1d48af3757d40
Julien Roncaglia <julien.roncaglia@itg.com>
2017-11-02 12:06:59 +0100

    Fix appveyor (hopefully)

--- build.cmd
+++ build.cmd
@@ -6,9 +6,4 @@ if errorlevel 1 (
   exit /b %errorlevel%
 )
 
-dir
-dir packages
-dir packages\build
-dir packages\build\FAKE
-dir packages\build\FAKE\tools\
 packages\build\FAKE\tools\FAKE.exe build.fsx %* --nocache
