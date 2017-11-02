096a048280b83cd1238ed3134e978ea6ec16e287
Julien Roncaglia <fox@vbfox.net>
2017-11-02 12:03:15 +0100

    Debug appveyor 2

--- build.cmd
+++ build.cmd
@@ -6,4 +6,9 @@ if errorlevel 1 (
   exit /b %errorlevel%
 )
 
+dir
+dir packages
+dir packages\build
+dir packages\build\FAKE
+dir packages\build\FAKE\tools\
 packages\build\FAKE\tools\FAKE.exe build.fsx %* --nocache
